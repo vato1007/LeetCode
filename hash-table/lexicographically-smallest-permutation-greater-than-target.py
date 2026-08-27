2589d62649ca0bbae2b35aa4874df7f85817d15f
vato <shrivathsa1551@gmail.com>
2026-08-27 11:27:07 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- hash-table/lexicographically-smallest-permutation-greater-than-target.py
+++ hash-table/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,43 +1,53 @@
 class Solution:
-    def lexGreaterPermutation(self, s: str, target: str) -> str:
+    def lexGreaterPermutation(self, s, target):
         cnt = [0] * 26
-        for c in s:
-            cnt[ord(c) - ord("a")] += 1
 
-        n = len(target)
-        res = []
+        for ch in s:
+            cnt[ord(ch) - 97] += 1
 
-        for i in range(n):
-            t = ord(target[i]) - ord("a")
+        for i in range(len(target)):
+            x = ord(target[i]) - 97
 
-            # Try placing the same character as target[i]
-            if cnt[t] > 0:
-                cnt[t] -= 1
-                # 检查能否成功
-                if self.can_greater(cnt, target[i + 1 :]):
-                    res.append(target[i])
-                    continue
-                cnt[t] += 1
+            if cnt[x] == 0:
+                break
 
-            # Find a larger character
-            for c in range(t + 1, 26):
+            cnt[x] -= 1
+
+            if i == len(target) - 1:
+                for j in range(i, -1, -1):
+                    y = ord(target[j]) - 97
+                    cnt[y] += 1
+
+                    for c in range(y + 1, 26):
+                        if cnt[c] > 0:
+                            cnt[c] -= 1
+                            suffix = ''.join(
+                                chr(k + 97) * cnt[k] for k in range(26)
+                            )
+                            return target[:j] + chr(c + 97) + suffix
+                return ""
+
+        # target prefix cannot be matched, so increase the first
+        # position that cannot be matched.
+        for c in range(x + 1, 26):
+            if cnt[c] > 0:
+                cnt[c] -= 1
+                suffix = ''.join(
+                    chr(k + 97) * cnt[k] for k in range(26)
+                )
+                return target[:i] + chr(c + 97) + suffix
+
+        # Backtrack if no direct increase is possible.
+        for j in range(i - 1, -1, -1):
+            y = ord(target[j]) - 97
+            cnt[y] += 1
+
+            for c in range(y + 1, 26):
                 if cnt[c] > 0:
                     cnt[c] -= 1
-                    res.append(chr(c + ord("a")))
-                    # Lexicographically smallest permutation of remaining characters
-                    res.append(
-                        "".join(chr(j + ord("a")) * cnt[j] for j in range(26))
+                    suffix = ''.join(
+                        chr(k + 97) * cnt[k] for k in range(26)
                     )
-                    return "".join(res)
-
-            # No feasible solution found
-            return ""
+                    return target[:j] + chr(c + 97) + suffix
 
         return ""
-
-    def can_greater(self, cnt: list[int], suffix: str) -> bool:
-        # Construct the largest string from largest to smallest
-        max_str = "".join(
-            chr(i + ord("a")) * cnt[i] for i in range(25, -1, -1) if cnt[i] > 0
-        )
-        return max_str > suffix
