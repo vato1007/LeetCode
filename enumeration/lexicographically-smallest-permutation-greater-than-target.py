c7993d671e348e6d24cbd3a292208b0aa8bfb287
vato <shrivathsa1551@gmail.com>
2026-08-27 11:26:51 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- enumeration/lexicographically-smallest-permutation-greater-than-target.py
+++ enumeration/lexicographically-smallest-permutation-greater-than-target.py
@@ -1,33 +1,43 @@
 class Solution:
-    def lexGreaterPermutation(self, s, target):
+    def lexGreaterPermutation(self, s: str, target: str) -> str:
         cnt = [0] * 26
+        for c in s:
+            cnt[ord(c) - ord("a")] += 1
 
-        for ch in s:
-            cnt[ord(ch) - 97] += 1
+        n = len(target)
+        res = []
 
-        used = 0
+        for i in range(n):
+            t = ord(target[i]) - ord("a")
 
-        for i in range(len(target)):
-            x = ord(target[i]) - 97
+            # Try placing the same character as target[i]
+            if cnt[t] > 0:
+                cnt[t] -= 1
+                # 检查能否成功
+                if self.can_greater(cnt, target[i + 1 :]):
+                    res.append(target[i])
+                    continue
+                cnt[t] += 1
 
-            if cnt[x] == 0:
-                break
-
-            cnt[x] -= 1
-            used += 1
-        else:
-            used = len(target)
-
-        for i in range(used - 1, -1, -1):
-            x = ord(target[i]) - 97
-            cnt[x] += 1
-
-            for c in range(x + 1, 26):
+            # Find a larger character
+            for c in range(t + 1, 26):
                 if cnt[c] > 0:
                     cnt[c] -= 1
-                    suffix = ''.join(
-                        chr(j + 97) * cnt[j] for j in range(26)
+                    res.append(chr(c + ord("a")))
+                    # Lexicographically smallest permutation of remaining characters
+                    res.append(
+                        "".join(chr(j + ord("a")) * cnt[j] for j in range(26))
                     )
-                    return target[:i] + chr(c + 97) + suffix
+                    return "".join(res)
+
+            # No feasible solution found
+            return ""
 
         return ""
+
+    def can_greater(self, cnt: list[int], suffix: str) -> bool:
+        # Construct the largest string from largest to smallest
+        max_str = "".join(
+            chr(i + ord("a")) * cnt[i] for i in range(25, -1, -1) if cnt[i] > 0
+        )
+        return max_str > suffix
