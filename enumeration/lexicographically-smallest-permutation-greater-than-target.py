5eaae93f8b1ebf492a4745dcd3dca58b1cd6d8d3
vato <shrivathsa1551@gmail.com>
2026-08-27 11:26:06 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

---
+++ enumeration/lexicographically-smallest-permutation-greater-than-target.py
@@ -0,0 +1,31 @@
+class Solution:
+    def lexGreaterPermutation(self, s, target):
+        cnt = [0] * 26
+
+        for ch in s:
+            cnt[ord(ch) - 97] += 1
+
+        for i in range(len(target)):
+            x = ord(target[i]) - 97
+
+            if cnt[x] > 0:
+                cnt[x] -= 1
+            else:
+                for c in range(x + 1, 26):
+                    if cnt[c] > 0:
+                        cnt[c] -= 1
+                        suffix = ''.join(chr(j + 97) * cnt[j] for j in range(26))
+                        return target[:i] + chr(c + 97) + suffix
+                break
+
+        for i in range(len(target) - 1, -1, -1):
+            x = ord(target[i]) - 97
+            cnt[x] += 1
+
+            for c in range(x + 1, 26):
+                if cnt[c] > 0:
+                    cnt[c] -= 1
+                    suffix = ''.join(chr(j + 97) * cnt[j] for j in range(26))
+                    return target[:i] + chr(c + 97) + suffix
+
+        return ""
