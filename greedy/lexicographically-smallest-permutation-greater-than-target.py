b0b26a016fd8ef6b6cd31c260e3d669ce88e4d48
vato <shrivathsa1551@gmail.com>
2026-08-27 11:26:29 +0530

    LC-lexicographically-smallest-permutation-greater-than-target [Runtime: N/A; Memory: N/A]

--- greedy/lexicographically-smallest-permutation-greater-than-target.py
+++ greedy/lexicographically-smallest-permutation-greater-than-target.py
@@ -5,27 +5,29 @@ class Solution:
         for ch in s:
             cnt[ord(ch) - 97] += 1
 
+        used = 0
+
         for i in range(len(target)):
             x = ord(target[i]) - 97
 
-            if cnt[x] > 0:
-                cnt[x] -= 1
-            else:
-                for c in range(x + 1, 26):
-                    if cnt[c] > 0:
-                        cnt[c] -= 1
-                        suffix = ''.join(chr(j + 97) * cnt[j] for j in range(26))
-                        return target[:i] + chr(c + 97) + suffix
+            if cnt[x] == 0:
                 break
 
-        for i in range(len(target) - 1, -1, -1):
+            cnt[x] -= 1
+            used += 1
+        else:
+            used = len(target)
+
+        for i in range(used - 1, -1, -1):
             x = ord(target[i]) - 97
             cnt[x] += 1
 
             for c in range(x + 1, 26):
                 if cnt[c] > 0:
                     cnt[c] -= 1
-                    suffix = ''.join(chr(j + 97) * cnt[j] for j in range(26))
+                    suffix = ''.join(
+                        chr(j + 97) * cnt[j] for j in range(26)
+                    )
                     return target[:i] + chr(c + 97) + suffix
 
         return ""
